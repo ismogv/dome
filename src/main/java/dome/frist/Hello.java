7bc83133dd9ea652a0e9de123d14bec232868638
quyan_glb <quyan_glb@git.com>
2020-06-03 17:26:56 +0800

    My frist git

---
+++ src/main/java/dome/frist/Hello.java
@@ -0,0 +1,13 @@
+package dome.frist;
+/**
+* @author 作者
+* @version 创建时间：2020年6月3日 下午4:07:51
+* 类说明
+*/
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("hello word!");
+	}
+
+}

95d05e2e76ab4f46c16df61a5479f7e1da61deb7
quyan_glb <quyan_glb@git.com>
2020-06-03 17:29:30 +0800

    add

---
+++ src/main/java/dome/frist/Test.java
@@ -0,0 +1,13 @@
+package dome.frist;
+/**
+* @author 作者
+* @version 创建时间：2020年6月3日 下午5:27:42
+* 类说明
+*/
+public class Test {
+	
+	public static void main(String[] args) {
+		 System.out.println("ceshi" );
+	}
+
+}
